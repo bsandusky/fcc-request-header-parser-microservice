bed1d594a5d8e35290b4d0bfe5b8743699546a01
Brett <brett.sandusky@gmail.com>
2017-02-03 01:57:33 +0000

    add req headers and output

--- app.js
+++ app.js
@@ -2,6 +2,18 @@
 const express = require("express")
 const app = express()
 
+app.get('/api/whoami', (req, res) => {
+    
+    console.log(req)
+    
+    let output = {  
+                    ipaddress: req.get("x-forwarded-for") || req.ip || req.socket.remoteAddress ||req.connection.socket.remoteAddress,
+                    language: req.get("accept-language").split(",")[0],
+                    software: req.get("user-agent").split(') ')[0].split(' (')[1]
+                }
+    res.send(output)
+})
+
 app.listen(process.env.PORT, () => {
     console.log("Server running on port " + process.env.PORT)
 })
