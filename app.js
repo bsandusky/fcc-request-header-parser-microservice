81bb778998fc27757dabb615c1f0c5f39b88243d
Brett <brett.sandusky@gmail.com>
2017-02-03 02:28:38 +0000

    update package.json

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.get('/whoami', (req, res) => {
     res.send(output)
 })
 
-app.listen(process.env.PORT || 8000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server running on port " + process.env.PORT)
 })
