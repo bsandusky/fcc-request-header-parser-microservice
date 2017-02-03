5e705f9923ee9b68ee56fc58546b0f27d8a73845
Brett <brett.sandusky@gmail.com>
2017-02-03 02:18:58 +0000

    update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 app.get('/whoami', (req, res) => {
 
     let output = {  
-                    ipaddress: req.get("x-forwarded-for") || req.ip || req.socket.remoteAddress ||req.connection.socket.remoteAddress,
+                    ipaddress: req.get("x-forwarded-for") || req.ip || req.socket.remoteAddress || req.connection.socket.remoteAddress,
                     language: req.get("accept-language").split(",")[0],
                     software: req.get("user-agent").split(') ')[0].split(' (')[1]
                 }
